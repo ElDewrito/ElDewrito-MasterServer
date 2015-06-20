039c5b8eaee91ea132c653e44d6c5e3a895ffc81
emoose <abc@cock.li>
2015-06-20 05:28:09 +0100

    change everything to use ugly JS-developer coding style so i can claim it uses less lines

--- node/index.js
+++ node/index.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     http = require('http'),
-    redis = require('redis'),
-    url = require('url');
+    redis = require('redis');
 
 var MAXIMUM_TIME_BEFORE_SERVER_PRONOUNCED_DEAD = 5 * 60;
 
@@ -10,28 +9,18 @@ var app = express();
 // use host entries created by Docker in /etc/hosts to connect to redis
 var client = redis.createClient('6379', 'redis');
 
-function jsonGet(options: any, callback: any)
-{
-    return http.get(options, function (resp)
-    {
+function jsonGet(options: any, callback: any) {
+    return http.get(options, function (resp) {
         var data = "";
         resp.on("data", function (chunk) { data += chunk; });
 
-        resp.on("end", function ()
-        {
-            callback(JSON.parse(data));
-        });
+        resp.on("end", function () { callback(JSON.parse(data)); });
 
-        resp.on("error", function (err)
-        {
-            data = "{\"error\": \"true\"}";
-            callback(JSON.parse(data));
-        });
+        resp.on("error", function (err) { callback({error: "true"}); });
     });
 }
 
-app.get('/announce', function (req, res)
-{
+app.get('/announce', function (req, res) {
     var shutdown = req.query.shutdown != undefined ? (req.query.shutdown == "true" || req.query.shutdown == "1") : false;
     var serverPort = req.query.port || 0;
     if (serverPort == 0)
@@ -40,11 +29,9 @@ app.get('/announce', function (req, res)
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var uri = ip + ":" + serverPort;
 
-    var req = jsonGet({ host: ip, path: "/", port: serverPort }, function (json)
-    {
+    var req = jsonGet({ host: ip, path: "/", port: serverPort }, function (json) {
         var isError = json.error != undefined ? json.error == "true" : false;
-        if (isError)
-        {
+        if (isError) {
             res.send({ result: { code: 2, msg: "Failed to retrieve server info JSON" } });
             return;
         }
@@ -52,8 +39,7 @@ app.get('/announce', function (req, res)
         var serverGamePort = json.port || 11774;
 
         var gamePortIsOpen = true; // todo: check if game port is accessible
-        if (!gamePortIsOpen)
-        {
+        if (!gamePortIsOpen) {
             res.send({ result: { code: 2, msg: "Failed to contact game server, are the ports open and forwarded correctly?" } });
             return;
         }
@@ -69,22 +55,17 @@ app.get('/announce', function (req, res)
     });
 });
 
-app.get("/list", function (req, res)
-{
+app.get("/list", function (req, res) {
     var returnData = { listVersion: 1, result: { code: 0, msg: "OK", servers: [] } };
-    client.smembers("servers", function (err, result)
-    {
-        if (!result || result.length <= 0)
-        {
+    client.smembers("servers", function (err, result) {
+        if (!result || result.length <= 0) {
             returnData.result.code = 1;
             returnData.result.msg = "No servers available";
             return res.send(returnData);
         }
 
-        result.forEach(function (uri)
-        {
-            client.hgetall(uri + ":info", function (err, obj)
-            {
+        result.forEach(function (uri) {
+            client.hgetall(uri + ":info", function (err, obj) {
                 var currentTime = Math.floor(Date.now() / 1000);
                 var lastUpdate = parseInt(obj.lastUpdate);
                 if (currentTime - lastUpdate < MAXIMUM_TIME_BEFORE_SERVER_PRONOUNCED_DEAD)
@@ -96,7 +77,6 @@ app.get("/list", function (req, res)
     });
 });
 
-http.createServer(app).listen(process.env.PORT || 8080, function ()
-{
+http.createServer(app).listen(process.env.PORT || 8080, function () {
     console.log('Listening on port ' + (process.env.PORT || 8080));
 });
