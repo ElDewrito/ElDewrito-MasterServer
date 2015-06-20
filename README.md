62be74679d13bea7029f0ec7c63ddb7114ef3ca0
emoose <abc@cock.li>
2015-06-20 09:05:47 +0100

    working master server implementation

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
-# A sample Docker workflow with Node.js, Redis and NGiNX
+# A sample master server implementation for ElDewrito
 
-[![Circle CI](https://circleci.com/gh/msanand/docker-workflow/tree/master.svg?style=shield)](https://circleci.com/gh/msanand/docker-workflow/tree/master)
+This implements all the features expected in an ElDewrito master server, in a stable, scalable, production-ready way.
 
-For a detailed explanation of the workflow used in this repository, refer to [this post from my blog](http://anandmanisankar.com/posts/docker-container-nginx-node-redis-example/):
-[http://anandmanisankar.com/posts/docker-container-nginx-node-redis-example/](http://anandmanisankar.com/posts/docker-container-nginx-node-redis-example/)
+I mainly only call it a sample as others can use this to see how endpoints should function, in case they want to implement their own master server.
 
-For a detailed overview of Containers and Docker, refer to [this post from my blog](http://anandmanisankar.com/posts/container-docker-PaaS-microservices/):
-[http://anandmanisankar.com/posts/container-docker-PaaS-microservices/](http://anandmanisankar.com/posts/container-docker-PaaS-microservices/)
+However it'll also work fine with next to no changes needed, the master server I'll be operating will run on the same code as this git.
 
+Installation (with Docker)
+=============
+- Install Docker
+- Install Docker-compose
+- Change to the main directory of this repo (with the docker-compose.yml file)
+- Run "docker-compose up"
+- Go have a coffee while it sets up the containers, once it's done you should be able to access the master server at port 80.
 
+This sets up and installs Redis, nginx and node.js automatically, each one running inside their own Docker container.
+
+By default it also sets up three separate instances of the main node.js application, with the nginx server set up to load balance between them.
+These instances are all linked to the main Redis database and share the same server list with each other.
+
+Installation (without Docker)
+=============================
+While Docker is the recommended way to set up the master server there are various ways you can set it up without using separate Docker containers.
+
+- First install Redis, this is required by the master server.
+- Also install node.js and NPM
+- Once they've been installed change to the node directory and run "npm install"
+- Sit back and wait for the dependencies to install
+- Run "node index.js" to start the master
+- The master can now be accessed at port 8080
+
+I recommend you setup a nginx forward proxy on port 80 to forward to the node.js application on port 8080, but if you don't want to you can just edit the index.js to run the node.js app on port 80 instead. (make sure to set isRunningBehindProxy to false if you do this!)
+
+Configuration
+=============
+You can edit various options in the config files inside each folder, node/index.js also has some options near the top of the file.
+
+You should set these config options before running "docker-compose up" for the first time, as changing things inside a Docker container can be troublesome.
+
+Note that ElDewrito is set up to re-announce to each master it knows about every 2.5 minutes, setting the serverContactTimeLimit to less than that will cause problems!
--- node/index.js
+++ node/index.js
@@ -1,86 +1,175 @@
+"use strict";
+// if a server hasn't contacted us in this many seconds it's assumed to be offline
+var serverContactTimeLimit = 5 * 60;
+
+// text sent to anyone who happens to contact the master server outside of ED
+var welcomeText = "<h1>Hi there.</h1>" +
+        "<p>This is a master server for ElDewrito (a Halo Online fan mod), to use this master server you'll need to use a server browser.</p>" +
+        "<h3>Useful links</h3>" +
+        "<ul><li><a href=\"https://www.reddit.com/r/HaloOnline\">/r/HaloOnline, the Halo Online subreddit</a> - Information about HaloOnline and ElDewrito can be found here.</li>" +
+        "<li><a href=\"https://forum.halo.click\">Halo.Click Forums</a> - The official forums for ElDewrito.</li><ul>" +
+        "<p>The source code for this master server can be downloaded from <a href=\"https://gitlab.com/ElDewrito/ElDewrito-MasterServer\">GitLab</a><p>";
+
+// if you're running this server behind a forward proxy (ie. a caching server, or with nginx hosting the frontend) set this to true
+// this will allow the master server to get the server IP from the X-Forwarded-For header instead of the remote_addr
+// which is needed since a forward proxy would have remote_addr set to the proxies IP, not the server which is contacting us
+// DISABLE THIS IF YOU'RE NOT USING A FORWARD PROXY, the X-Forwarded-For header can be easily spoofed if you don't have a proxy which is setting it
+// if someone spoofs it they could fill your master with dozens of fake announce requests!
+var isRunningBehindProxy = true;
+
+// the information for the redis server
+var redisHostName = "redis";
+var redisPortNumber = "6379";
+
+// end of configurable options, only edit below if you know what you're doing!
 var express = require('express'),
     http = require('http'),
-    redis = require('redis');
-
-var MAXIMUM_TIME_BEFORE_SERVER_PRONOUNCED_DEAD = 5 * 60;
+    request = require('request'),
+    redis = require('redis'),
+    async = require('async');
 
 var app = express();
 
 // use host entries created by Docker in /etc/hosts to connect to redis
-var client = redis.createClient('6379', 'redis');
+var client = redis.createClient(redisPortNumber, redisHostName);
 
-function jsonGet(options: any, callback: any) {
-    return http.get(options, function (resp) {
-        var data = "";
-        resp.on("data", function (chunk) { data += chunk; });
+function jsonGet(options, callback) {
+    return request(options, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            return callback({error: "true"});
+        }
 
-        resp.on("end", function () { callback(JSON.parse(data)); });
-
-        resp.on("error", function (err) { callback({error: "true"}); });
+        return callback(JSON.parse(body));
     });
 }
 
-app.get('/announce', function (req, res) {
-    var shutdown = req.query.shutdown != undefined ? (req.query.shutdown == "true" || req.query.shutdown == "1") : false;
-    var serverPort = req.query.port || 0;
-    if (serverPort == 0)
-        return res.send({ result: 1, msg: "Invalid parameters, valid parameters are 'port' (int) and 'shutdown' (bool)" });
+/*
+  /announce - used by game servers to announce their server to this master server
+  GET parameters:
+  - port (int) - port number of the JSON info server
+  - shutdown (bool) - specified if the server is shutting down, to let the master know to remove it
 
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    var uri = ip + ":" + serverPort;
-    
-    if(shutdown) { // server shutting down so delete its entries from redis
-        client.srem("servers", uri);
-        client.del(uri + ":info");
-        return res.send({ result: { code: 0, msg: "OK" } });
+  This route connects to $REMOTE_ADDR:$PORT and makes sure the JSON info server is accessible
+  then checks if the "port" (game port number) specified in the JSON is accessible
+  If they're both accessible then it adds the server to the Redis database (or updates the lastUpdate time if it already exists)
+
+  If shutdown is specified it'll remove the $REMOTE_ADDR:$PORT entry from Redis.
+
+  Game servers should keep contacting the /announce route every two minutes or so to make sure it's entry doesn't expire.
+
+  Returns a JSON object like below, letting the client know the status of the announce request
+  {
+    "result": {
+      "code": 0,
+      "msg": "OK"
+    }
+  }
+*/
+app.get('/announce', function (req, res) {
+    var shutdown = req.query.shutdown !== undefined ? (req.query.shutdown === "true" || req.query.shutdown === "1") : false;
+    var serverPort = req.query.port || 0;
+    if (serverPort === 0) {
+        return res.send({result: {code: 1, msg: "Invalid parameters, valid parameters are 'port' (int) and 'shutdown' (bool)"}});
     }
 
-    var req = jsonGet({ host: ip, path: "/", port: serverPort }, function (json) {
-        var isError = json.error != undefined ? json.error == "true" : false;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (!isRunningBehindProxy) {
+        ip = req.connection.remoteAddress;
+    }
+
+    var uri = ip + ":" + serverPort;
+
+    if (shutdown) { // server shutting down so delete its entries from redis
+        client.srem("servers", uri);
+        client.del(uri + ":info");
+        return res.send({result: {code: 0, msg: "Removed server from list"}});
+    }
+
+    jsonGet({uri: "http://" + uri + "/", timeout: 10 * 1000}, function (json) {
+        var isError = json.error !== undefined ? json.error === "true" : false;
         if (isError) {
-            res.send({ result: { code: 2, msg: "Failed to retrieve server info JSON" } });
-            return;
+            return res.send({result: {code: 2, msg: "Failed to retrieve server info JSON from " + uri}});
         }
 
         var serverGamePort = json.port || 11774;
 
-        var gamePortIsOpen = true; // todo: check if game port is accessible
+        var gamePortIsOpen = true; // to eee: check if game port is accessible
         if (!gamePortIsOpen) {
-            res.send({ result: { code: 2, msg: "Failed to contact game server, are the ports open and forwarded correctly?" } });
-            return;
+            return res.send({result: {code: 3, msg: "Failed to contact game server, are the ports open and forwarded correctly?"}});
         }
 
         // add ip to our servers set, if it already exists then it'll silently fail
-        client.sadd("servers", uri); 
+        client.sadd("servers", uri);
 
         // add/set the ip/port and current time to the db
-        client.hmset(uri + ":info", { "lastUpdate": Math.floor(Date.now() / 1000) });
-        //client.set(uri, Math.floor(Date.now() / 1000));
+        client.hmset(uri + ":info", {lastUpdate: Math.floor(Date.now() / 1000)});
 
-        res.send({ result: { code: 0, msg: "OK" } });
+        res.send({result: {code: 0, msg: "Added server to list"}});
     });
 });
 
+/*
+  /list - used by client server browsers to retrieve a list of game servers
+  GET parameters:
+  - maxListVersion (int) - max list version supported by the server browser (unimplemented)
+
+  List version 1 would just be a simple list of servers, version 2 may include some cached data about the server
+  To ensure masters will work with any server browser, no matter what listVersion they support, masters will have to honor the maxListVersion given
+  Alternatively we could just make the JSON reply always stay backwards compatible, ie. instead of changing any of the fields we'd only add new ones
+
+  This route simply goes through each entry in the "servers" Redis set, and then looks up the last update time for that entry
+  If the last update was less than MAXIMUM_TIME_BEFORE_SERVER_PRONOUNCED_DEAD then it's added to the returned JSON
+
+  Returns a JSON object like below, letting the client know the status of the request along with a list of servers
+  {
+    "listVersion": 1,
+    "result": {
+      "code": 0,
+      "msg": "OK",
+      "servers": [
+        "192.168.0.1:11775",
+        "127.0.0.1:11775"
+      ]
+    }
+  }
+*/
+
 app.get("/list", function (req, res) {
-    var returnData = { listVersion: 1, result: { code: 0, msg: "OK", servers: [] } };
+    var returnData = {listVersion: 1, result: {code: 0, msg: "OK", servers: []}};
+
     client.smembers("servers", function (err, result) {
-        if (!result || result.length <= 0) {
+        if (!result) {
             returnData.result.code = 1;
-            returnData.result.msg = "No servers available";
+            returnData.result.msg = "Unable to query database";
             return res.send(returnData);
         }
 
-        result.forEach(function (uri) {
+        function isServerAvailable(uri, callback) {
             client.hgetall(uri + ":info", function (err, obj) {
+                // can this be simplified? things i've read from ~2010 say this is the best way
+                if (err || typeof obj === undefined || !obj || typeof obj.lastUpdate === undefined || !obj.lastUpdate || obj.lastUpdate === 0) {
+                    return callback(false);
+                }
+
                 var currentTime = Math.floor(Date.now() / 1000);
                 var lastUpdate = parseInt(obj.lastUpdate);
-                if (currentTime - lastUpdate < MAXIMUM_TIME_BEFORE_SERVER_PRONOUNCED_DEAD)
-                    returnData.result.servers.push(uri);
-            });
-        });
+                if (currentTime - lastUpdate > serverContactTimeLimit) {
+                    return callback(false);
+                }
 
-        return res.send(returnData);
+                callback(true);
+            });
+        }
+
+        async.filter(result, isServerAvailable, function (results) {
+            returnData.result.servers = results;
+            return res.send(returnData);
+        });
     });
+});
+
+app.all("/", function (req, res) {
+    res.send(welcomeText);
 });
 
 http.createServer(app).listen(process.env.PORT || 8080, function () {
