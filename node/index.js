fba34481aa25683774db24a03c43af5bba4dcb4b
Capuccino <aguilarralph13@gmail.com>
2017-03-31 00:39:29 +0800

    standardize code (20%)

--- node/index.js
+++ node/index.js
@@ -1,25 +1,26 @@
-"use strict";
 // if a server hasn't contacted us in this many seconds it's assumed to be offline
-var serverContactTimeLimit = 5 * 60;
+const serverContactTimeLimit = 5 * 60;
 
 // text sent to anyone who happens to contact the master server outside of ED
-var welcomeText = "<h1>Hi there.</h1>" +
-    "<p>This is a master server for ElDewrito (a Halo Online fan mod), to use this master server you'll need to use a server browser.</p>" +
-    "<h3>Useful links</h3>" +
-    "<ul><li><a href=\"https://www.reddit.com/r/HaloOnline\">/r/HaloOnline, the Halo Online subreddit</a> - Information about HaloOnline and ElDewrito can be found here.</li>" +
-    "<li><a href=\"https://forum.halo.click\">Halo.Click Forums</a> - The official forums for ElDewrito.</li><ul>" +
-    "<p>The source code for this master server can be downloaded from <a href=\"https://github.com/ElDewrito/ElDewrito-MasterServer\">GitHub</a><p>";
+var welcomeText; 
+welcomeText = '<h1>Hi there.</h1>'; 
+welcomeText =  "<p>This is a master server for ElDewrito (a Halo Online fan mod), to use this master server you'll need to use a server browser.</p>"; 
+welcomeText =  '<h3>Useful links</h3>'; 
+welcomeText =  '<ul><li><a href="https://www.reddit.com/r/HaloOnline">/r/HaloOnline, the Halo Online subreddit</a> - Information about HaloOnline and ElDewrito can be found here.</li>'; 
+welcomeText =  '<li><a href="https://forum.halo.click">Halo.Click Forums</a> - The official forums for ElDewrito.</li><ul>'; 
+welcomeText =  '<p>The source code for this master server can be downloaded from <a href="https://github.com/ElDewrito/ElDewrito-MasterServer">GitHub</a><p>';
 
-// if you're running this server behind a forward proxy (ie. a caching server, or with nginx hosting the frontend) set this to true
-// this will allow the master server to get the server IP from the X-Forwarded-For header instead of the remote_addr
-// which is needed since a forward proxy would have remote_addr set to the proxies IP, not the server which is contacting us
-// DISABLE THIS IF YOU'RE NOT USING A FORWARD PROXY, the X-Forwarded-For header can be easily spoofed if you don't have a proxy which is setting it
-// if someone spoofs it they could fill your master with dozens of fake announce requests!
-var isRunningBehindProxy = true;
+/* if you're running this server behind a forward proxy (ie. a caching server, or with nginx hosting the frontend) set this to true
+* this will allow the master server to get the server IP from the X-Forwarded-For header instead of the remote_addr
+* which is needed since a forward proxy would have remote_addr set to the proxies IP, not the server which is contacting us
+* DISABLE THIS IF YOU'RE NOT USING A FORWARD PROXY, the X-Forwarded-For header can be easily spoofed if you don't have a proxy which is setting it
+* if someone spoofs it they could fill your master with dozens of fake announce requests!
+*/
+const isRunningBehindProxy = true;
 
 // the information for the redis server, the defaults here correspond with the /etc/hosts entries created by Docker
-var redisHostName = "redis";
-var redisPortNumber = "6379";
+var redisHostName = 'redis';
+var redisPortNumber = '6379';
 
 // the default port number for this application, if you're using the Docker install you should leave this as 8080 and edit the nginx config instead
 var appPortNumber = process.env.PORT || 8080;
@@ -40,19 +41,19 @@ var app = express();
 var client = redis.createClient(redisPortNumber, redisHostName);
 
 function jsonGet(options, callback) {
-    return request(options, function(error, response, body) {
+    return request(options, (error, response, body) => {
         if (error || response.statusCode !== 200) {
             return callback({
-                error: "true"
+                error: 'true'
             });
         }
         var data;
         try {
             data = JSON.parse(body);
-        } catch (ex) {
-            console.log("error contacting", options.uri, ":", ex);
+        } catch(ex) {
+            console.log('error contacting', options.uri, ':', ex);
             data = {
-                error: "true"
+                error: 'true'
             };
         }
         return callback(data);
@@ -81,8 +82,8 @@ function jsonGet(options, callback) {
     }
   }
 */
-app.get('/announce', function(req, res) {
-    var shutdown = req.query.shutdown === "true" || req.query.shutdown == 1;
+app.get('/announce', (req, res) => {
+    var shutdown = req.query.shutdown === 'true' || req.query.shutdown == 1;
 
     if (!req.query.port) {
         return res.send({
@@ -97,7 +98,7 @@ app.get('/announce', function(req, res) {
         return res.send({
             result: {
                 code: 4,
-                msg: "Invalid port. A valid port is in the range 1024-65535."
+                msg: 'Invalid port. A valid port is in the range 1024-65535.'
             }
         }); //could allow 1-65535
     }
@@ -112,35 +113,35 @@ app.get('/announce', function(req, res) {
         return res.send({
             result: {
                 code: 5,
-                msg: "Invalid IP address."
+                msg: 'Invalid IP address.'
             }
         }); //unlikely
     }
 
-    var uri = ip + ":" + serverPort;
+    var uri = ip + ':' + serverPort;
 
     if (shutdown) { // server shutting down so delete its entries from redis
-        client.srem("servers", uri);
-        client.del(uri + ":info");
-        console.log("Removed server", uri); //you wanted to actually log this, right?
+        client.srem('servers', uri);
+        client.del(uri + ':info');
+        console.log('Removed server', uri); //you wanted to actually log this, right?
         return res.send({
             result: {
                 code: 0,
-                msg: "Removed server from list"
+                msg: 'Removed server from list'
             }
         });
     }
 
     jsonGet({
-        uri: "http://" + uri + "/",
+        uri: 'http://' + uri + '/',
         timeout: 10 * 1000
-    }, function(json) {
-        var isError = json.error !== undefined ? json.error === "true" : false;
+    }, json => {
+        var isError = json.error !== undefined ? json.error === 'true' : false;
         if (isError) {
             return res.send({
                 result: {
                     code: 2,
-                    msg: "Failed to retrieve server info JSON from " + uri
+                    msg: 'Failed to retrieve server info JSON from ' + uri
                 }
             });
         }
@@ -151,39 +152,39 @@ app.get('/announce', function(req, res) {
             return res.send({
                 result: {
                     code: 4,
-                    msg: "Server returned invalid port. A valid port is in the range 1024-65535."
+                    msg: 'Server returned invalid port. A valid port is in the range 1024-65535.'
                 }
             }); //maybe should have unique code, idk
         }
 
-        var gamePortIsOpen = true; // todo: check if game port is accessible
+        const gamePortIsOpen = true; // todo: check if game port is accessible
         if (!gamePortIsOpen) {
             return res.send({
                 result: {
                     code: 3,
-                    msg: "Failed to contact game server, are the ports open and forwarded correctly?"
+                    msg: 'Failed to contact game server, are the ports open and forwarded correctly?'
                 }
             });
         }
 
         // add ip to our servers set, if it already exists then it'll silently fail
-        client.sadd("servers", uri);
+        client.sadd('servers', uri);
 
         // add/set the ip/port and current time to the db
-        client.hmset(uri + ":info", {
+        client.hmset(uri + ':info', {
             lastUpdate: Math.floor(Date.now() / 1000)
         });
 
         res.send({
             result: {
                 code: 0,
-                msg: "Added server to list"
+                msg: 'Added server to list'
             }
         });
-        console.log("Added server", uri);
+        console.log('Added server', uri);
     });
 });
-
+//REVIEW NEEDED: Is this supposed to be JSDoc?
 /*
   /list - used by client server browsers to retrieve a list of game servers
   GET parameters:
@@ -210,30 +211,30 @@ app.get('/announce', function(req, res) {
   }
 */
 
-app.get("/list", function(req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+app.get('/list', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     var returnData = {
         listVersion: 1,
         result: {
             code: 0,
-            msg: "OK",
+            msg: 'OK',
             servers: []
         }
     };
 
-    client.smembers("servers", function(err, result) {
+    client.smembers('servers', (err, result) => {
         if (!result) {
             returnData.result.code = 1;
-            returnData.result.msg = "Unable to query database";
+            returnData.result.msg = 'Unable to query database';
             return res.send(returnData);
         }
 
         function isServerAvailable(uri, callback) {
-            client.hgetall(uri + ":info", function(err, obj) {
+            client.hgetall(uri + ':info', (err, obj) => {
                 // can this be simplified? things i've read from ~2010 say this is the best way
                 if (err || typeof obj === undefined || !obj || typeof obj.lastUpdate === undefined || !obj.lastUpdate || obj.lastUpdate === 0) {
                     return callback(false);
@@ -249,14 +250,14 @@ app.get("/list", function(req, res) {
             });
         }
 
-        async.filter(result, isServerAvailable, function(results) {
+        async.filter(result, isServerAvailable, results => {
             returnData.result.servers = results;
             return res.send(returnData);
         });
     });
 });
 
-app.all("/", function(req, res) {
+app.all('/', (req, res) => {
     res.send(welcomeText);
 });
 
@@ -315,26 +316,26 @@ var jsonParser = bodyParser.json();
     }
   }
 */
-app.post("/stats", jsonParser, function(req, res) {
+app.post('/stats', jsonParser, (req, res) => {
     function ReformatKey(isPrivateKey, key) {
         var pos = 0;
-        var returnKey = "";
+        var returnKey = '';
         while (pos < key.length) {
             var toCopy = key.length - pos;
             if (toCopy > 64)
                 toCopy = 64;
             returnKey += key.substr(pos, toCopy);
-            returnKey += "\n";
+            returnKey += '\n';
             pos += toCopy;
         }
-        var keyType = (isPrivateKey ? "RSA PRIVATE KEY" : "PUBLIC KEY"); // public keys don't have RSA in the name some reason
-        return "-----BEGIN " + keyType + "-----\n" + returnKey + "-----END " + keyType + "-----\n";
+        var keyType = (isPrivateKey ? 'RSA PRIVATE KEY' : 'PUBLIC KEY'); // public keys don't have RSA in the name some reason
+        return '-----BEGIN ' + keyType + '-----\n' + returnKey + '-----END ' + keyType + '-----\n';
     }
     if (!isRunningStatsServer)
         return res.send({
             result: {
                 code: 1,
-                msg: "Stats are unsupported on this master server"
+                msg: 'Stats are unsupported on this master server'
             }
         });
 
@@ -342,21 +343,21 @@ app.post("/stats", jsonParser, function(req, res) {
         return res.send({
             result: {
                 code: 2,
-                msg: "Invalid stats data"
+                msg: 'Invalid stats data'
             }
         });
 
     var pubKey = ReformatKey(false, req.body.publicKey);
 
-    var verifier = crypto.createVerify("RSA-SHA256");
+    var verifier = crypto.createVerify('RSA-SHA256');
     verifier.update(req.body.stats);
-    var isValidSig = verifier.verify(pubKey, req.body.signature, "base64");
+    var isValidSig = verifier.verify(pubKey, req.body.signature, 'base64');
 
     if (!isValidSig) {
         return res.send({
             result: {
                 code: 3,
-                msg: "Stats signature invalid"
+                msg: 'Stats signature invalid'
             }
         });
     }
@@ -385,11 +386,11 @@ app.post("/stats", jsonParser, function(req, res) {
     res.send({
         result: {
             code: 0,
-            msg: "OK"
+            msg: 'OK'
         }
     });
 });
 
-http.createServer(app).listen(appPortNumber, "0.0.0.0", function() {
+http.createServer(app).listen(appPortNumber, '0.0.0.0', () => {
     console.log('Listening on port ' + appPortNumber);
 });
